b524fe70fc4e870a238aec62205d070baddf19c2
Tharushan <Tharushan@users.noreply.github.com>
2023-08-11 20:05:22 +0000

    feat: start django project

---
+++ todoum/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from todoum.users.models import User
+from todoum.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
